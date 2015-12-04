8dd6b5b43d0f46e0fa101c247569f315cdac455b
Marius Darila <marius.darila@gmail.com>
2015-12-04 15:29:38 +0200

    updates

---
+++ README.md
@@ -0,0 +1,9 @@
+# This package is for [ethereum](https://ethereum.org/) dapp development
+
+## What's does this package:
+    * Exports a global class named `GethConnector` available only on **server**
+    * Downloads geth binaries
+    * Can start geth process
+    * Can connect to an ipc geth process and send  [json-rpc](http://https://github.com/ethereum/wiki/wiki/JSON-RPC#json-rpc-methods) call
+
+## How to use it
--- package.js
+++ package.js
@@ -1,12 +1,12 @@
-const packageVersion  = '0.0.1';
+const packageVersion = '0.0.1';
 
 Package.describe({
-  name:          'akasha:akasha-geth',
+  name:          'akasha:meteor-geth',
   version:       packageVersion,
   // Brief, one-line summary of the package.
-  summary:       '',
+  summary:       'ethereum geth for meteor dapps',
   // URL to the Git repository containing the source code for this package.
-  git:           '',
+  git:           'https://github.com/AkashaProject/meteor-geth.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -16,11 +16,13 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.use('sanjo:long-running-child-process@1.1.3');
-  api.use('akasha:fs-extra@0.26.2');
-  api.use('akasha:request@2.67.0');
-  api.use('akasha:shelljs@0.5.3');
-  api.use('akasha:adm-zip@0.4.7');
+  api.use('check', 'server');
+  api.use('sanjo:long-running-child-process@1.1.3', 'server');
+  api.use('akasha:fs-extra@0.26.2', 'server');
+  api.use('akasha:request@2.67.0', 'server');
+  api.use('akasha:shelljs@0.5.3', 'server');
+  api.use('akasha:adm-zip@0.4.7', 'server');
+  api.use('practicalmeteor:loglevel@1.1.0_2', 'server')
   api.addFiles(['lib/geth.js'], 'server');
   api.export('GethConnector', 'server');
 });
